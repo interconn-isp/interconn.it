7d1bd9ff02090db10a59f18f058fca4d685d7823
Alessandro Desantis <desa.alessandro@gmail.com>
2014-07-10 16:43:43 +0200

    Improve laxy loading of partner logos

--- app/assets/javascripts/pages/home/partner_list.js
+++ app/assets/javascripts/pages/home/partner_list.js
@@ -1,6 +1,9 @@
 (function() {
   $(window).load(function() {
-    $('.partner-list img').lazyload();
+    $('.partner-list img').lazyload({
+      threshold: 400,
+      container: $('.partner-list')
+    });
 
     $('.partner-list').marquee({
       duration: 10000,
