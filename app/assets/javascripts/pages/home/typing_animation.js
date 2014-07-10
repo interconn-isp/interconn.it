1e9ada3b4818e330ee66d0946bc0d44b6d652292
Alessandro Desantis <desa.alessandro@gmail.com>
2014-07-10 16:14:18 +0200

    Adjust typing animation speed

--- app/assets/javascripts/pages/home/typing_animation.js
+++ app/assets/javascripts/pages/home/typing_animation.js
@@ -2,9 +2,9 @@
   $(function() {
     if ($("#home-title-animated").is(":visible")) {
       $("#home-slogan").typed({
-        strings: ["la tua famiglia.", "^1000 la tua azienda.", "^1000 le tue idee.", "^1000 te."],
-        typeSpeed: 50,
-        backSpeed: 50,
+        strings: ["la tua famiglia.", "la tua azienda.", "le tue idee.", "te."],
+        typeSpeed: 30,
+        backSpeed: 30,
         startDelay: 1000,
         backDelay: 1000,
       });
