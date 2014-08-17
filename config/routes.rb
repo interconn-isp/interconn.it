a28059bd56182990cefa5495cd0365f9c057670c
Alessandro Desantis <desa.alessandro@gmail.com>
2014-08-17 15:18:17 +0200

    Add Press kit

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
   root to: 'home#show'
 
+  get 'coverage', to: 'high_voltage/pages#show', id: 'coverage'
+  get 'contract', to: 'high_voltage/pages#show', id: 'contract'
+  get 'careers', to: 'high_voltage/pages#show', id: 'careers'
+  get 'about', to: 'high_voltage/pages#show', id: 'about'
+  get 'press-kit', to: 'high_voltage/pages#show', id: 'press_kit'
+
+  get 'contact', to: 'contacts#new'
+  post 'contact', to: 'contacts#create'
+
   get 'pricing', to: 'high_voltage/pages#show', id: 'pricing/wadsl'
   get 'pricing/wadsl', to: 'high_voltage/pages#show', id: 'pricing/wadsl'
   get 'pricing/whdsl', to: 'high_voltage/pages#show', id: 'pricing/whdsl'
   get 'pricing/voip', to: 'high_voltage/pages#show', id: 'pricing/voip'
-
-  get 'coverage', to: 'high_voltage/pages#show', id: 'coverage'
-
-  get 'contact', to: 'contacts#new'
-  post 'contact', to: 'contacts#create'
 
   get 'faq', to: 'high_voltage/pages#show', id: 'faq/terms'
   get 'faq/terms', to: 'high_voltage/pages#show', id: 'faq/terms'
   get 'faq/tech', to: 'high_voltage/pages#show', id: 'faq/tech'
   get 'faq/voip', to: 'high_voltage/pages#show', id: 'faq/voip'
   get 'faq/condos', to: 'high_voltage/pages#show', id: 'faq/condos'
-
-  get 'contract', to: 'high_voltage/pages#show', id: 'contract'
-  get 'careers', to: 'high_voltage/pages#show', id: 'careers'
-  get 'about', to: 'high_voltage/pages#show', id: 'about'
 
   resources :inquiries, only: [:create]
   resource :inquiry, only: [:edit, :update]
