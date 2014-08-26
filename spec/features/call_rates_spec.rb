a1394d585716996003445bd5ef4b4c196e5800e0
Alessandro Desantis <desa.alessandro@gmail.com>
2014-08-26 20:32:46 +0200

    Fix failing spec

--- spec/features/call_rates_spec.rb
+++ spec/features/call_rates_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'the call rate consultation page', type: :feature do
-  before(:each) do
+  it 'displays the search modal', js: true do
     visit pricing_voip_path
     click_link 'questo modulo.'
-  end
 
-  it 'displays the search modal', js: true do
     expect(page).to have_content('Tariffe VoIP')
   end
 
   it 'displays the matching call rates', js: true do
+    visit pricing_voip_path
+    click_link 'questo modulo.'
+
     FactoryGirl.create(:call_rate, {
       traffic_direction: 'Foobar',
-      rate: 0.026,
+      rate: 0.0266,
       time_slot: :flat
     })
 
@@ -22,7 +23,7 @@ RSpec.describe 'the call rate consultation page', type: :feature do
 
     within '#call-rates-table' do
       expect(page).to have_content 'Foobar'
-      expect(page).to have_content '€0,03/min'
+      expect(page).to have_content '€0,027/min'
     end
   end
 end
