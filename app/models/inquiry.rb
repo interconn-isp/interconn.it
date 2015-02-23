caded2503523fff5fb61d959287cf99629128ab8
Alessandro Desantis <desa.alessandro@gmail.com>
2015-02-23 18:02:18 +0100

    Load plans from ActiveYaml in Inquiry model

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,11 +1,7 @@
 class Inquiry < ActiveRecord::Base
   extend Enumerize
 
-  enumerize :product, in: %w(
-    wadsl-7mega-privati wadsl-10mega-privati wadsl-4mega-business
-    wadsl-7mega-business whdsl-2mega-1mega whdsl-2mega-2mega whdsl-4mega-2mega
-    voip
-  )
+  enumerize :product, in: Plan.all.map(&:slug)
 
   validates :full_name, presence: true
   validates :email, presence: true, format: { with: /@/ }
