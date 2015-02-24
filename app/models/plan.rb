baf7c78f8a8dd33b8ed7f717b5f11f8dfaa82d8e
Alessandro Desantis <desa.alessandro@gmail.com>
2015-02-24 10:42:04 +0100

    Use full names as labels for product select in inquiry page

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,6 +1,7 @@
 class Plan < ActiveYaml::Base
   field :category_id
   field :name
+  field :full_name
   field :slug
   field :vat_included, default: true
   field :monthly_cost
@@ -10,4 +11,8 @@ class Plan < ActiveYaml::Base
   include ActiveYaml::Aliases
 
   belongs_to :category, class_name: 'PlanCategory', foreign_key: 'category_id'
+
+  def full_name
+    "#{category.name} #{name}"
+  end
 end
