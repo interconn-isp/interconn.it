71c34dc4d3435c2e0c8d9af29c762ad7c9ce5c02
Alessandro Desantis <desa.alessandro@gmail.com>
2015-02-24 18:20:49 +0100

    Add testimonials

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -2,4 +2,8 @@ class Testimonial < ActiveYaml::Base
   field :customer_name
   field :customer_image
   field :text
+
+  def self.with_customer_image
+    all.select{ |t| t.customer_image.present? }
+  end
 end
