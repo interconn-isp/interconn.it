00102a453ce0eb2e015f874bed138c7624486bdb
Alessandro Desantis <desa.alessandro@gmail.com>
2016-04-25 15:04:27 +0200

    Test all pricing plan pages

--- spec/features/plans_spec.rb
+++ spec/features/plans_spec.rb
@@ -1,9 +1,36 @@
 RSpec.feature 'The pricing plans page' do
-  scenario 'Viewing the plans' do
+  scenario 'Viewing the WADSL plans' do
     visit root_path
     first(:link, 'Piani').click
 
     expect(page).to have_content 'WADSL'
+  end
+
+  scenario 'Viewing the WHDSL plans' do
+    visit root_path
+    first(:link, 'Piani').click
+
+    click_link 'WHDSL'
+
+    expect(page).to have_content 'WHDSL'
+  end
+
+  scenario 'Viewing the ADSL plans' do
+    visit root_path
+    first(:link, 'Piani').click
+
+    click_link 'ADSL'
+
+    expect(page).to have_content 'ADSL'
+  end
+
+  scenario 'Viewing the optic fiber plans' do
+    visit root_path
+    first(:link, 'Piani').click
+
+    click_link 'Fibra'
+
+    expect(page).to have_content 'Fibra'
   end
 
   scenario 'Viewing the VoIP plan' do
