5f5cae00493f9a74b8077c7c64fe95fda0487313
Alessandro Desantis <desa.alessandro@gmail.com>
2018-02-10 13:00:24 +0100

    Handle plan not found with 404

--- app/controllers/plan_categories_controller.rb
+++ app/controllers/plan_categories_controller.rb
@@ -9,6 +9,7 @@ class PlanCategoriesController < ApplicationController
 
   def show
     @category = PlanCategory.find_by_slug(params[:id])
+    fail ActiveRecord::RecordNotFound unless @category
     respond_with @category
   end
 end
