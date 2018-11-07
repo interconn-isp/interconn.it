f5d78dd6421585b217bea22f712b7d56559551e0
Alessandro Desantis <desa.alessandro@gmail.com>
2018-11-07 21:46:53 +0100

    Adopt Heroku pipelines

--- .circleci/setup-heroku.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote add heroku-staging git@heroku.com:interconn-v1-staging.git
-git remote add heroku-production git@heroku.com:interconn-v1.git
-
-cat >> ~/.ssh/config << EOF
-  VerifyHostKeyDNS yes
-  StrictHostKeyChecking no
-EOF
