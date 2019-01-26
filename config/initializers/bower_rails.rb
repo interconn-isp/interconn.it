c4c24683c1944635a0cacc5ef3d4f56d9c4e4b5c
Alessandro Desantis <desa.alessandro@gmail.com>
2019-01-26 17:52:50 +0100

    Disable Bower on asset precompilation

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -3,11 +3,11 @@ BowerRails.configure do |bower_rails|
   bower_rails.root_path = Dir.pwd
 
   # Invokes rake bower:install before precompilation. Defaults to false
-  bower_rails.install_before_precompile = true
+  bower_rails.install_before_precompile = false
 
   # Invokes rake bower:resolve before precompilation. Defaults to false
-  bower_rails.resolve_before_precompile = true
+  bower_rails.resolve_before_precompile = false
 
   # Invokes rake bower:clean before precompilation. Defaults to false
-  bower_rails.clean_before_precompile = true
+  bower_rails.clean_before_precompile = false
 end if defined?(BowerRails)
